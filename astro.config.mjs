40d976eb5ad281f8353fc7845401219b3029917e
Santiago Cézar <santiagocezar2013@gmail.com>
2022-12-18 22:33:33 -0300

    iconify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
+//@ts-check
 import { defineConfig } from 'astro/config';
+import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
-
+    vite: {
+        plugins: [
+            Icons({
+                compiler: 'astro',
+            }),
+        ],
+    },
 });
